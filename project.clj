68d907e8b2eb20996d5bea6e0749769ee7a019d3
Brenton Ashworth <bashwort@gmail.com>
2010-11-19 10:34:36 -0800

    Added push script. Update README.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject clj-diff "1.0.0-SNAPSHOT"
   :description "Sequential diff in Clojure."
+  :url "http://github.com/brentonashworth/clj-diff"
   :dependencies [[org.clojure/clojure "1.2.0"]]
   ;; All dev dependencies are for performance testing.
   :dev-dependencies [[incanter "1.2.3"]
