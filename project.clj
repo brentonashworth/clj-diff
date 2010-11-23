5c755087e134c64a29c8a3f0eaed3d0995cfdab0
Brenton Ashworth <bashwort@gmail.com>
2010-11-23 10:38:21 -0800

    Move performance code to new project.

--- project.clj
+++ project.clj
@@ -1,7 +1,4 @@
 (defproject clj-diff "1.0.0-SNAPSHOT"
   :description "Sequential diff in Clojure."
   :url "http://github.com/brentonashworth/clj-diff"
-  :dependencies [[org.clojure/clojure "1.2.0"]]
-  ;; All dev dependencies are for performance testing.
-  :dev-dependencies [[incanter "1.2.3"]
-                     [org.clojars.brenton/google-diff-match-patch "0.1"]])
+  :dev-dependencies [[org.clojure/clojure "1.2.0"]])
