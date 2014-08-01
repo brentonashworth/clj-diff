60a1b2b733a2ec710cd41d40223e94549253c135
Joel Holdbrooks <cjholdbrooks@gmail.com>
2014-07-31 23:15:36 -0700

    Update project file

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
 (defproject clj-diff "1.0.0-SNAPSHOT"
   :description "Sequential diff in Clojure."
   :url "http://github.com/brentonashworth/clj-diff"
-  :source-path "src/clj"
+  :source-paths ["src/clj" "src/cljs"]
   :java-source-path "src/jvm"
   :java-fork "true"
   :java-debug "true"
   :hooks [leiningen.hooks.javac
           leiningen.hooks.difftest]
-  :dev-dependencies [[org.clojure/clojure "1.2.0"]
+  :dev-dependencies [[org.clojure/clojure "1.6.0"]
                      [lein-javac "1.2.1-SNAPSHOT"]
                      [marginalia "0.5.0"]
                      [lein-difftest "1.3.2-SNAPSHOT"]])
