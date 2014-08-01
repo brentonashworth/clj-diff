957d55ad2eecbfbfaf7d580c62052e0927b2e2cf
Joel Holdbrooks <cjholdbrooks@gmail.com>
2014-07-31 23:14:02 -0700

    Direct port to ClojureScript

---
+++ src/cljs/clj_diff/core.cljs
@@ -0,0 +1,116 @@
+(ns clj-diff.core
+  "Diff, patch and related functions for Clojure sequences."
+  (:require [clj-diff.miller :as miller]))
+
+(defn diff
+  "Create the edit script for transforming sequance a into sequence b.
+  An edit script is a map with keys :+ and :- for additions and deletions.
+  Additions are represented as a sequence of vectors. The first item in each
+  vector is the index where the rest of the items in the vector are to be
+  inserted. For example [3 b c] means to insert b an c after whatever is
+  in index 3. Deletions are represented as a sequence of indexes to delete.
+
+  For example: the diff of 'abcabba' and 'cbabac' would generate the edit
+  script below.
+
+      {:+ [[2 b] [6 c]], :- [0 1 5]}
+
+  An index of -1 may appear in additions and is a special case which means to
+  add the elements at the beginning of the sequence."
+  [a b]
+  (miller/diff a b))
+
+(defn- merge-patch
+  [s edit-script delete-symbol]
+  (let [s (vec s)
+        additions (:+ edit-script)
+        deletions (:- edit-script)
+        s (reduce (fn [a b]
+                    (assoc a b delete-symbol))
+                  s
+                  deletions)
+        s (reduce (fn [a b]
+                    (let [index (first b)
+                          items (rest b)]
+                      (if (= index -1)
+                        (assoc a 0 (conj (vec items) (get a 0)))
+                        (assoc a index (conj items (get a index))))))
+                  s
+                  additions)]
+    (flatten s)))
+
+(defn patch*
+  [s edit-script]
+  (filter #(not (nil? %)) (merge-patch s edit-script nil)))
+
+(defmulti ^{:arglists '([s edit-script])} patch
+  "Use the instructions in the edit script to transform the sequence s into
+  a new sequence. If the edit script was created by using diff on a and b then
+  patch will use the edit script to transform a into b.
+
+  (diff a b) -> x, (patch a x) -> b."
+  (fn [s _] (type s)))
+
+(defmethod patch :default
+  [s edit-script]
+  (patch* s edit-script))
+
+(defmethod patch js/String
+  [s edit-script]
+  (apply str (patch* s edit-script)))
+
+(defn edit-distance
+  "Returns the edit distance between the two passed sequences. May also be
+  passed an edit script. The edit distance is the minimum number of insertions
+  and deletions required to transform one sequence into another."
+  ([a b]
+     (miller/edit-distance a b))
+  ([edit-script]
+     (+ (count (:- edit-script))
+        (reduce + (map #(count (drop 1 %)) (:+ edit-script))))))
+
+(defn- max-or-zero [coll]
+  (if (and (coll? coll)
+           (not (empty? coll)))
+    (apply max coll)
+    0))
+
+(defn levenshtein-distance
+  "Returns the Levenshtein distance between two sequences. May either be passed
+  the two sequences or a diff of the two sequences.
+
+  From [Wikipedia](http://en.wikipedia.org/wiki/Levenshtein_distance):
+  The Levenshtein distance between two strings is the minimum number of edits
+  needed to transform one string into the other, with the allowable edit
+  operations being insertion, deletion and substitution of a single character.
+
+  This function works not only with strings but with any Clojure sequence.
+
+  Warning! Technically this function is estimating the Levenshtein distance
+  from a computed diff. Most of the time, it is the same as the real Levenshtein
+  distance but in same cases it may be larger. The reason for this is that
+  there may be multiple paths through an edit graph with the same edit
+  distance but with differing Levenshtein distance. A future improvement to
+  the diff algorithm whould be to find all paths and prefer the one with the
+  minimum Levenshtein distance."
+  ([a b]
+     (levenshtein-distance (diff a b)))
+  ([edit-script]
+     (let [additions (map #(let [index (first %)
+                                 items (rest %)]
+                             (apply vector index (repeat (count items) :a)))
+                          (:+ edit-script))
+           max-index (max (max-or-zero (map first additions))
+                          (max-or-zero (:- edit-script)))
+           v (vec (repeat max-index :e))
+           patched (merge-patch v (merge edit-script {:+ additions}) :d)
+           edit-groups (filter #(not= :e (first %))
+                               (partition-by #(if (= % :e) :e :edit)
+                                             patched))]
+       (reduce + (map (fn [group]
+                        (max (count (filter #(= % :a) group))
+                             (count (filter #(= % :d) group))))
+                      edit-groups)))))
+
+(defn longest-common-subseq [a b]
+  (miller/longest-common-subseq a b))
---
+++ src/cljs/clj_diff/optimizations.cljs
@@ -0,0 +1,163 @@
+(ns clj-diff.optimizations
+  "String optimizations for diff algorithms.
+  See http://neil.fraser.name/writing/diff/.")
+
+(defn- fast-common-prefix [^String a ^String b]
+  (let [n (min (.-length a) (.-length b))]
+    (loop [i 0]
+      (if (<= i n)
+        (if (not= (aget a i)
+                  (aget b i))
+          i
+          (recur (inc i)))
+        n))))
+
+(defn- fast-common-suffix [^String a ^String b]
+  (let [la (.-length a)
+        lb (.-length b)
+        n (min la lb)]
+    (loop [i 1]
+      (if (<= i n)
+        (if (not= (aget a (- la i))
+                  (aget b (- lb i)))
+          (dec i)
+          (recur (inc i)))
+        n))))
+
+(defn common-prefix [^String a ^String b]
+  (let [i (fast-common-prefix a b)]
+    [i (.substring a i) (.substring b i)]))
+
+(defn common-suffix [^String a ^String b]
+  (let [i (fast-common-suffix a b)]
+    [i
+     (.substring a 0 (- (.-length a) i))
+     (.substring b 0 (- (.-length b) i))]))
+
+(defn- short-within-long
+  "Return a diff if the shorter sequence exists in the longer one. No need to
+  use the expensive diff algorithm for this."
+  [^String a ^String b ca cb]
+  (let [[short long] (if (> ca cb) [b a] [a b])
+        i (int (.indexOf long short))]
+    (if (= i -1)
+      nil
+      (if (= short a)
+        {:+ (filter #(not (nil? %))
+                    [(when (> i 0)
+                       (vec (concat [-1] (seq (.substring b 0 i)))))
+                     (when (< (+ i ca) cb)
+                       (vec (concat [(dec (+ i ca))]
+                                    (seq (.substring b (+ i ca))))))])
+         :- []}
+        {:+ []
+         :- (vec (concat (range 0 i)
+                         (range (+ i cb) ca)))}))))
+
+(defn- half-match* [^String long ^String short i]
+  (let [target (.substring long i (+ i (quot (count long) 4)))]
+    (loop [j (.indexOf short target 0)
+           result []]
+      (if (= j -1)
+        (if (>= (count (or (first result) ""))
+                (quot (count long) 2))
+          result
+          nil)
+        (let [prefix-length (first (common-prefix (.substring long i)
+                                                  (.substring short j)))
+              suffix-length (first (common-suffix (.substring long 0 i)
+                                                  (.substring short 0 j)))
+              common (or (first result) "")]
+          (recur (.indexOf short target (inc j))
+                 (if (< (count common) (+ prefix-length suffix-length))
+                   [(str (.substring short (- j suffix-length) j)
+                         (.substring short j (+ j prefix-length)))
+                    (.substring long 0 (- i suffix-length))
+                    (.substring long (+ i prefix-length))
+                    (.substring short 0 (- j suffix-length))
+                    (.substring short (+ j prefix-length))]
+                   result)))))))
+
+(defn- half-match
+  "Find a substring shared by both sequences which is at least half as long
+  as the longer sequence. Return a vector of five elements if one is found and
+  and nil if not. The five elements are: the common sequence, the prefix
+  of sequence a, the suffix of sequence a, the prefix of sequence b and the
+  suffix of sequence b."
+  [^String a ^String b]
+  (let [[short long] (if (> (count a) (count b)) [b a] [a b])
+        short-count (count short)
+        long-count (count long)]
+    (if (or (< long-count 4)
+            (< (* short-count 2) long-count))
+      nil
+      (let [hm-second-q (half-match* long short (quot (+ long-count 3) 4))
+            hm-third-q (half-match* long short (quot (+ long-count 1) 2))
+            half-match (cond (and hm-second-q hm-third-q)
+                             (if (> (count (first hm-second-q))
+                                    (count (first hm-third-q)))
+                               hm-second-q
+                               hm-third-q)
+                             :else (or hm-second-q hm-third-q))]
+        (cond (nil? half-match) nil
+              (= a long) half-match
+              :else [(get half-match 0)
+                     (get half-match 3)
+                     (get half-match 4)
+                     (get half-match 1)
+                     (get half-match 2)])))))
+
+(defn- offset-diffs [diffs offset]
+  {:+ (vec (map #(apply vector
+                        (+ offset (first %)) (rest %)) (:+ diffs)))
+   :- (vec (map #(+ offset %) (:- diffs)))})
+
+(declare diff)
+
+(defn- diff*
+  "Calculate the diff using the function f only after ensuring that this
+  algorithm is required. At this point we know that a and b are different at
+  both ends. A diff can be calculated manually if the length of a or b is 0
+  or if the smaller of the two sequences is contained within the longer."
+  [^String a ^String b f]
+  (let [ca (count a)
+        cb (count b)]
+    (or (cond (= ca 0) {:+ [(vec (concat [-1] (seq b)))]
+                        :- []}
+              (= cb 0) {:+ []
+                        :- (vec (range 0 ca))}
+              :else (if-let [diffs (short-within-long a b ca cb)]
+                      diffs
+                      (if-let [half-match (half-match a b)]
+                        (let [common (get half-match 0)
+                              a-prefix (get half-match 1)
+                              a-suffix (get half-match 2)
+                              b-prefix (get half-match 3)
+                              b-suffix (get half-match 4)
+                              diff-a (diff a-prefix b-prefix f)
+                              diff-b (diff a-suffix b-suffix f)]
+                          (merge-with concat
+                                      diff-a
+                                      (offset-diffs diff-b
+                                                    (+ (count common)
+                                                       (count a-prefix)))))
+                        nil)))
+        (f a b))))
+
+(defn diff
+  "Return the diff of a and b. Wrap the diff function f in pre and post
+  optimizations. Check for nil and equality. Remove common prefix and suffix."
+  [^String a ^String b f]
+  (let [diffs (cond (or (nil? a) (nil? b))
+                    (throw (IllegalArgumentException. "Cannot diff nil."))
+                    (= a b) {:+ [] :- []}
+                    :else nil)]
+    (or diffs
+        (let [[prefix a b] (common-prefix a b)
+              [suffix a b] (common-suffix a b)
+              diffs (diff* a b f)]
+          (if (> prefix 0)
+            {:+ (vec (map #(apply vector
+                                  (+ prefix (first %)) (rest %)) (:+ diffs)))
+             :- (vec (map #(+ prefix %) (:- diffs)))}
+            diffs)))))
