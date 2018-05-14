69535924bf87fe4f57ec91089d11dec6fb58b0b4
Andrew Nichols <andrew.nichols@droitfintech.com>
2018-05-14 13:26:36 -0400

    Add license to project.

--- project.clj
+++ project.clj
@@ -1,6 +1,11 @@
 (defproject tech.droit/clj-diff "1.0.0"
   :description "Sequential diff in Clojure."
   :url "http://github.com/droitfintech/clj-diff"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
+
   :source-paths ["src/clj"]
   :java-source-paths ["src/jvm"]
   :plugins [[lein-marginalia "0.9.1"]]
